02a2f54c1c23487b1510946d708c8b8b77b59d6e
Chandpa Khushal <chandpakhushal6@gmail.com>
2026-05-02 23:35:25 +0530

    Remove console branding banner

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
-import { showConsoleBrand } from "./lib/consoleBrand.js";
-
-showConsoleBrand();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
